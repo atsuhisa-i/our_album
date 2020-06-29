850199833b309360b288e0328eefedd844c985e7
atsuhisa-i <atsuhisa.1124@gmail.com>
2020-06-29 22:18:18 +0900

    usersコントローラとviewの作成、編集
    ユーザーマイページに関するコントローラとview

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
     get "/users/sign_out", :to => "users/sessions#destroy"
   end
   
+  resources :users, only: [:index]
   root 'tops#index'
   # resources :tops, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
