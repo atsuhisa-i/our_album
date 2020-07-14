221a8ee0ea4d9703607a7b4f1ff6f2f38815b475
atsuhisa-i <atsuhisa.1124@gmail.com>
2020-07-14 20:17:34 +0900

    グループ編集ページの実装2

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -20,9 +20,16 @@ class GroupsController < ApplicationController
   end
 
   def edit
+    @group = Group.find(params[:id])
   end
 
   def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to users_path, notice: 'グループを更新しました'
+    else
+      render :edit
+    end
   end
 
 private
