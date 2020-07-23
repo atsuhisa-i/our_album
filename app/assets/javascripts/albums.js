1ebea23a8d1604c9447ed146f478ef634831bdc0
atsuhisa-i <atsuhisa.1124@gmail.com>
2020-07-23 17:13:41 +0900

    アルバム検索4

--- app/assets/javascripts/albums.js
+++ app/assets/javascripts/albums.js
@@ -1,4 +1,28 @@
 $(function() {
+  function addAlbum(album) {
+    let html = `
+      <div class="album__picture">
+        <img src=${album.picture} width="220" height="150">
+      </div>
+      <div class="album__title">
+        タイトル：${album.title}
+      </div>
+      <div class="album__date">
+        日付：${album.date}
+      </div>
+    `;
+    $(".albums-search__result").append(html);
+  }
+
+  function addNoAlbum() {
+    let html = `
+      <div class="no-album">
+        <p class="no-album__content">アルバムが見つかりません</p>
+      </div>
+    `;
+    $(".albums-search__result").append(html);
+  }
+
   $("#albums-search").on("keyup", function() {
     let input = $("#albums-search").val();
     $.ajax({
@@ -23,4 +47,4 @@ $(function() {
         alert("通信エラーです。アルバムが表示できません。")
       });
   });
-});
+})
