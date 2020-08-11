a9ebc0c080eb506d7b29e50d3c65ca9c56227eeb
atsuhisa-i <atsuhisa.1124@gmail.com>
2020-08-11 14:19:33 +0900

    swiperの導入4

--- app/assets/javascripts/swiper.js
+++ app/assets/javascripts/swiper.js
@@ -1,44 +1,37 @@
 $(function() {
-  var galleryThumbs = new Swiper('.gallery-thumbs', {
-    spaceBetween: 10,
-    slidesPerView: 4,
+  new Swiper('.swiper-container', {
+  // Optional parameters
+    // direction: 'vertical',
     loop: true,
-    freeMode: true,
-    loopedSlides: 5, //looped slides should be the same
-    watchSlidesVisibility: true,
-    watchSlidesProgress: true,
-  });
-  var galleryTop = new Swiper('.gallery-top', {
-    spaceBetween: 10,
-    loop: true,
-    loopedSlides: 5, //looped slides should be the same
+    grabCursor: true,
+    effect: 'coverflow',
+    centeredSlides: true,
+    slidesPerView: 1,
+    speed: 1000,
+    breakpoints: {
+      1024: {
+        slidesPerView: 2,
+      }
+    },
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false
+    },
+
+  // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+  // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
-    thumbs: {
-      swiper: galleryThumbs,
+
+  // And if we need_rollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
     },
-  });
-  // new Swiper('.swiper-container', {
-  // Optional parameters
-    // direction: 'vertical',
-    // loop: true,
-
-  // If we need pagination
-    // pagination: {
-    //   el: '.swiper-pagination',
-    // },
-
-  // Navigation arrows
-    // navigation: {
-    //   nextEl: '.swiper-button-next',
-    //   prevEl: '.swiper-button-prev',
-    // },
-
-  // And if we need scrollbar
-    // scrollbar: {
-    //   el: '.swiper-scrollbar',
-    // },
-  // })
+  })
 })
