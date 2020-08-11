7214be37bf651fcf4ac6e478b5cea7172060f5ee
atsuhisa-i <atsuhisa.1124@gmail.com>
2020-08-11 12:07:15 +0900

    swiperの導入3

--- app/assets/javascripts/swiper.js
+++ app/assets/javascripts/swiper.js
@@ -1,23 +1,44 @@
 $(function() {
-  new Swiper('.swiper-container', {
-  // Optional parameters
-    // direction: 'vertical',
+  var galleryThumbs = new Swiper('.gallery-thumbs', {
+    spaceBetween: 10,
+    slidesPerView: 4,
     loop: true,
-
-  // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-
-  // Navigation arrows
+    freeMode: true,
+    loopedSlides: 5, //looped slides should be the same
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+  });
+  var galleryTop = new Swiper('.gallery-top', {
+    spaceBetween: 10,
+    loop: true,
+    loopedSlides: 5, //looped slides should be the same
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
+    thumbs: {
+      swiper: galleryThumbs,
+    },
+  });
+  // new Swiper('.swiper-container', {
+  // Optional parameters
+    // direction: 'vertical',
+    // loop: true,
+
+  // If we need pagination
+    // pagination: {
+    //   el: '.swiper-pagination',
+    // },
+
+  // Navigation arrows
+    // navigation: {
+    //   nextEl: '.swiper-button-next',
+    //   prevEl: '.swiper-button-prev',
+    // },
 
   // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  })
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    // },
+  // })
 })
